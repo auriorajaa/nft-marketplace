00b0766db23c628e1efd4e678c6974d07b380267
Aurio Rajaa <riorajaa2018@gmail.com>
2024-04-26 13:34:19 +0700

    finishing...

--- src/script.js
+++ src/script.js
@@ -26,12 +26,10 @@ var swiper1 = new Swiper('.slide-content1', {
     },
 });
 
-
 var swiper2 = new Swiper(".slide-container2", {
     slidesPerView: 3,
     spaceBetween: 10,
     sliderPerGroup: 3,
-    loop: true,
     centerSlide: "true",
     fade: "true",
     grabCursor: "true",
@@ -43,6 +41,39 @@ var swiper2 = new Swiper(".slide-container2", {
     navigation: {
         nextEl: ".swiper-button-next2",
         prevEl: ".swiper-button-prev2",
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        1000: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+var swiper3 = new Swiper(".slide-container3", {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    sliderPerGroup: 3,
+    centerSlide: "true",
+    fade: "true",
+    grabCursor: "true",
+    pagination: {
+        el: ".swiper-pagination3",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next3",
+        prevEl: ".swiper-button-prev3",
     },
 
     breakpoints: {
